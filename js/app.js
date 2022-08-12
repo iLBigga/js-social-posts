fe67d1dcb2986726024820fad68324b7342514f0
iLBigga <Giannanti1996@gmail.com>
2022-08-12 12:26:41 +0200

    Aggiunto index

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const posts = [
+    {
+        id : 1,
+        autore : {
+            nome : 'Pablo Escobar',
+            foto : 'https://picsum.photos/70/70',
+        },
+        data :  '08-05-2022',
+        contenuto : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, non.',
+        immagine : 'https://picsum.photos/300/200',
+        like : 50,
+    },
+    {
+        id : 2,
+        autore : {
+            nome : 'George Jung',
+            foto : 'https://picsum.photos/70/70',
+        },
+        data :  '08-05-2022',
+        contenuto : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, non.',
+        immagine : 'https://picsum.photos/300/200',
+        like : 50,
+    },
+    {
+        id : 3,
+        autore : {
+            nome : 'Saul Goodman',
+            foto : 'https://picsum.photos/70/70',
+        },
+        data :  '08-05-2022',
+        contenuto : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, non.',
+        immagine : 'https://picsum.photos/300/200',
+        like : 50,
+    },
+];
+
